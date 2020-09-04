60b790cb1f5e65197a58c5a19fd65bcd4e9a2edb
Taiwo <akinlabitaiwo73@gmail.com>
2020-09-04 14:35:17 -0700

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from  django.urls import path
+from basic_app import views
+
+# TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other')
+]
